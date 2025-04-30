5a784fcdee0bd46cba877cc8369aca8d61d865c6
Bernardo Rocha da Silva <programadorbernardords@gmail.com>
2025-04-30 10:08:35 -0400

    Atualização 2 do Software

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Heading } from './components/Heading';
-
 import "./styles/theme.css";
 import "./styles/global.css";
 
-export function App() {
-  console.log("Oi");
+import { Container } from './components/Container/index';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
+export function App() {
   return (
     <>
-      <Heading attr={123} attr2="String Qualquer">Olá Mundo1</Heading>
-      <Heading>Olá Mundo2</Heading>
-      <Heading>Olá Mundo3</Heading>
-      <Heading>Olá Mundo4</Heading>
-      <Heading>Olá Mundo5</Heading>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum eligendi error, totam quidem eos quam?</p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
