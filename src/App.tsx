beec5823f048e76288e5f93a94552dc799e71563
Bernardo Rocha da Silva <programadorbernardords@gmail.com>
2025-05-09 15:43:26 -0400

    Software Atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/global.css";
 import { Container } from './components/Container/index';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown'
 
 export function App() {
   return (
@@ -14,6 +15,32 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className='form'>
+          <div className='formRow'>
+            <label htmlFor="input">Task</label>
+            <input id='input' type="text" />
+          </div>
+          
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolores.</p>
+          </div>
+          
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formrow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
